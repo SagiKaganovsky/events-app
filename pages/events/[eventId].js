e51da96216c355c789451234274b4b62fcb0fb87
Sagi Kaganovsky <sergkagan@gmail.com>
2023-04-16 18:42:07 +0300

    event search form and layout added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,3 +1,6 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
 import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
 
@@ -11,7 +14,20 @@ const EventDetailPage = () => {
     return <p>No event found!</p>;
   }
 
-  return <>{event.title}</>;
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default EventDetailPage;
